92482aab7d04164f132a57f6250254818b0702c7
Ben Avison <code@piccolosystems.com>
2026-02-06 11:32:46 +0000

    Fudge to allow testing on Linux with unlicensed version of toolchain

--- Modules/Compiler/IAR-C.cmake
+++ Modules/Compiler/IAR-C.cmake
@@ -30,12 +30,7 @@ set(CMAKE_C${CMAKE_C_STANDARD_COMPUTED_DEFAULT}_EXTENSION_COMPILE_OPTION -e)
 
 # Architecture specific
 if("${CMAKE_C_COMPILER_ARCHITECTURE_ID}" STREQUAL "ARM")
-  if (CMAKE_C_COMPILER_VERSION VERSION_LESS 5)
-    # IAR C Compiler for Arm prior version 5.xx uses XLINK. Support in CMake is not implemented.
-    message(FATAL_ERROR "IAR C Compiler for Arm version ${CMAKE_C_COMPILER_VERSION} not supported by CMake.")
-  endif()
   __compiler_iar_ilink(C)
-  __compiler_check_default_language_standard(C 5.10 90 6.10 99 8.10 11 8.40 17)
 
 elseif("${CMAKE_C_COMPILER_ARCHITECTURE_ID}" STREQUAL "RX")
   __compiler_iar_ilink(C)
--- Modules/Compiler/IAR-CXX.cmake
+++ Modules/Compiler/IAR-CXX.cmake
@@ -20,12 +20,7 @@ if(NOT CMAKE_IAR_CXX_FLAG)
   cmake_policy(SET CMP0057 NEW) # if IN_LIST
 
   set(_CMAKE_IAR_MODERNCXX_LIST 14 17)
-  if(${CMAKE_CXX_STANDARD_COMPUTED_DEFAULT} IN_LIST _CMAKE_IAR_MODERNCXX_LIST OR
-     ("${CMAKE_CXX_COMPILER_ARCHITECTURE_ID}" STREQUAL "ARM" AND ${CMAKE_CXX_STANDARD_COMPUTED_DEFAULT} EQUAL 98))
-    string(PREPEND CMAKE_CXX_FLAGS "--c++ ")
-  else()
     string(PREPEND CMAKE_CXX_FLAGS "--eec++ ")
-  endif()
   unset(_CMAKE_IAR_MODERNCXX_LIST)
 
   cmake_policy(POP)
@@ -39,12 +34,7 @@ set(CMAKE_CXX${CMAKE_CXX_STANDARD_COMPUTED_DEFAULT}_EXTENSION_COMPILE_OPTION -e)
 
 # Architecture specific
 if("${CMAKE_CXX_COMPILER_ARCHITECTURE_ID}" STREQUAL "ARM")
-  if (CMAKE_CXX_COMPILER_VERSION VERSION_LESS 5)
-    # IAR C++ Compiler for Arm prior version 5.xx uses XLINK. Support in CMake is not implemented.
-    message(FATAL_ERROR "IAR C++ Compiler for Arm version ${CMAKE_CXX_COMPILER_VERSION} not supported by CMake.")
-  endif()
   __compiler_iar_ilink(CXX)
-  __compiler_check_default_language_standard(CXX 5.10 98 8.10 14 8.40 17)
 
 elseif("${CMAKE_CXX_COMPILER_ARCHITECTURE_ID}" STREQUAL "RX")
   __compiler_iar_ilink(CXX)
